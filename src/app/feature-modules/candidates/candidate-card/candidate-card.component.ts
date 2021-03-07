aa27ee767001c67df2f1b9bda2d81488a92e19ee
Naveen Kumar <naveen-sed.kumar@broadcom.com>
2021-03-07 10:56:22 +0530

    responsive changes and list card component changes

--- src/app/feature-modules/candidates/candidate-card/candidate-card.component.ts
+++ src/app/feature-modules/candidates/candidate-card/candidate-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-candidate-card',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./candidate-card.component.scss']
 })
 export class CandidateCardComponent implements OnInit {
+  
+  @Input() candidate : any;
 
   constructor() { }
 
